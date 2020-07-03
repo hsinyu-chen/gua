63bc083801d9b903366cf3e3941ea2b40ed48ebe
陳信羽 <evaimpact@outlook.com>
2020-07-04 01:15:51 +0800

    v4

--- service-worker.js
+++ service-worker.js
@@ -3,19 +3,19 @@ self.addEventListener('install', function (event) {
         caches.open('web').then(function (cache) {
             return cache.addAll(
                 [
-                    '/',
-                    '/index.html',
-                    '/dist/main.css',
-                    '/dist/scripts/main.js',
-                    '/android-chrome-192x192.png',
-                    '/android-chrome-512x512.png',
-                    '/apple-touch-icon.png',
-                    '/favicon.ico',
-                    '/favicon-16x16.png',
-                    '/favicon-32x32.png',
-                    '/manifest.json',
-                    '/mstile-150x150.png',
-                    '/safari-pinned-tab.svg',
+                    '/gua/',
+                    '/gua/index.html',
+                    '/gua/dist/main.css',
+                    '/gua/dist/scripts/main.js',
+                    '/gua/android-chrome-192x192.png',
+                    '/gua/android-chrome-512x512.png',
+                    '/gua/apple-touch-icon.png',
+                    '/gua/favicon.ico',
+                    '/gua/favicon-16x16.png',
+                    '/gua/favicon-32x32.png',
+                    '/gua/manifest.json',
+                    '/gua/mstile-150x150.png',
+                    '/gua/safari-pinned-tab.svg',
                 ]
             );
         })
